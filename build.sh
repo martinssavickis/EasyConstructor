6a5692d51bc174d526a88a2fcc00ca6ddccf410e
morowinder <morowinder@gmail.com>
2018-03-16 22:33:47 +0000

    this is weird but all should work now

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 dotnet restore EasyConstructor.sln
+#I haven't figured out how to get msbuild on my machine, so override full framework logation with mono for now
+#use this to run locally
+#export FrameworkPathOverride=/usr/lib/mono/4.5/
 msbuild build src/EasyConstructor.csproj
 dotnet test test/test.csproj
